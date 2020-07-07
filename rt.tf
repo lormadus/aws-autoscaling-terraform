5a6a3be2656e751aa429ecafd2946b426b1daabb
David Yoon <hsyoon@microsoft.com>
2020-07-07 16:23:33 +0900

    Update rt.tf

--- rt.tf
+++ rt.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "dev_public" {
-  vpc_id = "${aws_vpc.dev.id}"
+  vpc_id = aws_vpc.dev.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.dev.id}"
+    gateway_id = aws_internet_gateway.dev.id
   }
 
   tags = {
@@ -12,11 +12,11 @@ resource "aws_route_table" "dev_public" {
 }
 
 resource "aws_route_table_association" "dev_public_1a" {
-  subnet_id      = "${aws_subnet.public_1a.id}"
-  route_table_id = "${aws_route_table.dev_public.id}"
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.dev_public.id
 }
 
 resource "aws_route_table_association" "dev_public_1c" {
-  subnet_id      = "${aws_subnet.public_1c.id}"
-  route_table_id = "${aws_route_table.dev_public.id}"
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.dev_public.id
 }
