2734ae9605cd14f74d4f52a6c4f6e4e325210d4e
David Yoon <hsyoon@microsoft.com>
2020-07-07 16:26:23 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "public_1a" {
   vpc_id            = aws_vpc.dev.id
+  ## vpc_id            = "vpc-026af350fc64456e3"  // 직접 VPC ID로 넣어도 동작
   availability_zone = "ap-southeast-1a"
   cidr_block        = "172.16.1.0/24"
 
