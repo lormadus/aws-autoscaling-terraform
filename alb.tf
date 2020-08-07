fa4d5fc6f34c4ddc28e4dad019cf8c13568023d0
David Yoon <hsyoon@microsoft.com>
2020-08-07 10:58:39 +0900

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -31,6 +31,8 @@ resource "aws_alb_target_group" "frontend" {
     }
     tags = { Name = "user06-Frontend Target Group" }
 }
+
+## 개별 인스턴스에 연결하는 경우 아래 부분 추가
 ##resource "aws_alb_target_group_attachment" "frontend" {
 ##    target_group_arn = "${aws_alb_target_group.frontend.arn}"
 ##    target_id = "${aws_instance.bastion_1a.id}"
